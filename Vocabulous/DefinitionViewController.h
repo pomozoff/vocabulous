008728cc40eab243657835272c47c703bdda3a2b
Антон Помозов <apomozov@apomozov.local>
2011-06-09 10:24:07 +0400

    Initial commit

---
+++ Vocabulous/DefinitionViewController.h
@@ -0,0 +1,16 @@
+//
+//  DefinitionViewController.h
+//  Vocabulous
+//
+//  Created by Антон on 08.06.11.
+//  Copyright 2011 Штрих-М. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface DefinitionViewController : UIViewController {
+    
+}
+
+@end
